993761e7f311e3509a31622ac437715a201d666f
Victoria Hale <vic.hale74@gmail.com>
2019-11-21 19:28:38 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
+  
+  create_table :users do |t|
+    t.string :name 
+    t.tring :email 
+    t.string :password
+  end
 end
