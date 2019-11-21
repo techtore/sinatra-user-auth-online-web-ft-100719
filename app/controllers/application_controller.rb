3e6b7fd65b7313ecc39f45fe3ee4524f27078ab8
Victoria Hale <vic.hale74@gmail.com>
2019-11-21 20:35:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,7 @@ class ApplicationController < Sinatra::Base
   get '/sessions/login' do
 
     # the line of code below render the view page in app/views/sessions/login.erb. renders login form
+    
     erb :'sessions/login'
   end
 
